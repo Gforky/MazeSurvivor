6049702270f7c6571f4307e90f0ad1ff6cb32de1
LukeMiao <lwmusc2012@gmail.com>
2015-04-19 22:31:23 -0700

    Now the GenerateRandomMaze class can generate a random maze with type and 4 corner vertices; the MazeSurvivorRenderer can create and draw a 8x8 maze

--- app/src/main/java/com/series/survivor/survivorgames/GenerateRandomMaze.java
+++ app/src/main/java/com/series/survivor/survivorgames/GenerateRandomMaze.java
@@ -10,16 +10,23 @@ public class GenerateRandomMaze {
 
     //Inner class to store the cell type and the vertices coordinates of the cell
     static class Cell {
-        public final char Type;
-        public final float[] coords;
+        public char Type;
+        public float[] coords;
 
         public Cell(char Type, float[] coords) {
             this.Type = Type;
             this.coords = coords;
         }
-
     }
-    public char[][] generateMaze(int row, int col) {
+
+    public Cell[][] generateMaze(int row, int col) {
+
+        //Initialize the maze cells
+        Cell[][] maze = new Cell[row][col];//store the cells in maze
+        //get the upper left corner vertex's X and Y coordinates
+        float leftMost = -((row / 2) * 0.01f);
+        float upMost = (col / 2) * 0.01f;
+
         //using char matrix to save space
         if(row < 1 || col < 1) {//Sanity check
             return null;
@@ -27,23 +34,35 @@ public class GenerateRandomMaze {
         //Randomly choose a start point in maze
         int startX = (int)(Math.random() * (row - 1));
         int startY = (int) (Math.random() * (col - 1));
-        char[][] maze = new char[row][col];//int matrix to store the maze
+
         //initialize the maze
+        float temp = leftMost;//temporally store the left most X coordinate
         for(int r = 0; r < row; r++) {
             for(int c = 0; c < col; c++) {
+                //initialize the corresponding cells
                 if(r == startX && c == startY) {//initially only set the start point as available path
-                    maze[r][c] = 'p';//p as "path"
+                    maze[r][c] = new Cell('p', null);//p as "path"
                 } else {//set all the other cells as walls
-                    maze[r][c] = 'w';//w as "wall"
+                    maze[r][c] = new Cell('w', null);//w as "wall"
                 }
+                //set the four vertices of the cell
+                maze[r][c].coords = new float[]{
+                        leftMost, upMost, 0.0f,//top left
+                        leftMost, upMost - 0.02f, 0.0f,//bottom left
+                        leftMost + 0.02f, upMost - 0.02f, 0.0f,//bottom right
+                        leftMost + 0.02f, upMost, 0.0f//top right
+                };
+                leftMost += 0.02f;//move to next column of the maze
             }
+            upMost -= 0.02f;//move to next row of the maze
+            leftMost = temp;
         }
         generate(maze, startX, startY);//generate a maze with the matrix and start point
         return maze;
     }
 
     //Recursion: at each level, move the path two steps to a random direction, if valid
-    private void generate(char[][] maze, int X, int Y) {
+    private void generate(Cell[][] maze, int X, int Y) {
         Dir[] dirs = Dir.values();//get the array of four directions
         shuffle(dirs);//shuffle the order of the directions, in order to move to random direction at each level
         for(Dir dir : dirs) {//try every direction, check whether can move to
@@ -51,8 +70,8 @@ public class GenerateRandomMaze {
             int nextX = dir.moveX(dir.moveX(X));
             int nextY = dir.moveY(dir.moveY(Y));
             if(valid(maze, nextX, nextY)) {
-                maze[dir.moveX(X)][dir.moveY(Y)] = 'p';
-                maze[nextX][nextY] = 'p';
+                maze[dir.moveX(X)][dir.moveY(Y)].Type = 'p';
+                maze[nextX][nextY].Type = 'p';
                 generate(maze, nextX, nextY);//continue generate the path from the new cell
             }//end-if
         }//end-for
@@ -70,8 +89,8 @@ public class GenerateRandomMaze {
     }
 
     //check if the destination cell is valid to move to
-    private boolean valid(char[][] maze, int x, int y) {
-        if(x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] != 'p') {//cell is with the maze, and is not be dig yet
+    private boolean valid(Cell[][] maze, int x, int y) {
+        if(x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y].Type != 'p') {//cell is with the maze, and is not be dig yet
             return true;
         }
         return false;
--- app/src/main/java/com/series/survivor/survivorgames/MazeSurvivorRenderer.java
+++ app/src/main/java/com/series/survivor/survivorgames/MazeSurvivorRenderer.java
@@ -14,16 +14,14 @@ public class MazeSurvivorRenderer implements GLSurfaceView.Renderer {
     GenerateRandomMaze mazeGenerator;
 
     //Storage to store the cell information in the maze, 'w' mean wall, 'p' means path, 's' means survivor
-    char[][] maze;
+    GenerateRandomMaze.Cell[][] maze;
+    MazeCell[][] mazeCells;
 
-    //All 3 parameters needed to draw the maze
-    private MazeCell MazeWall;
+    //All 3 colors needed to draw the maze
     private final float[] wallColor =  new float[]{1.0f, 0.0f, 0.0f, 1.0f };
 
-    private MazeCell MazePath;
     private final float[] pathColor = new float[]{0.0f, 1.0f, 0.0f, 1.0f};
 
-    private MazeCell MazeSurvivor;
     private final float[] survivorColor = new float[]{0.0f, 0.0f, 1.0f, 1.0f};
 
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
@@ -33,17 +31,17 @@ public class MazeSurvivorRenderer implements GLSurfaceView.Renderer {
 
         //Get the maze from generator
         maze = mazeGenerator.generateMaze(8, 8);//generate a M * N maze
+        mazeCells = new MazeCell[8][8];
 
         //set the background frame color
         gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
         //Set the vertices coordinates for all the cells in the maze
-        MazeWall = new MazeCell(new float[]{
-                -0.05f, 0.05f, 0.0f,//top left
-                -0.05f, -0.05f, 0.0f,//bottom left
-                0.05f, -0.05f, 0.0f,//bottom right
-                0.05f, 0.05f, 0.0f}//top right
-                );
+        for(int r = 0; r < 8; r++) {
+            for(int c = 0; c < 8; c++) {
+                mazeCells[r][c] = new MazeCell(maze[r][c].coords);
+            }
+        }
     }
 
     public void onDrawFrame(GL10 gl) {
@@ -51,7 +49,17 @@ public class MazeSurvivorRenderer implements GLSurfaceView.Renderer {
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 
         //draw the maze
-        MazeWall.draw(gl, wallColor);
+        for(int r = 0; r < 8; r++) {
+           for(int c = 0; c < 8; c++) {
+               if(maze[r][c].Type == 'w') {//draw the wall cell
+                   mazeCells[r][c].draw(gl, wallColor);
+               } else if(maze[r][c].Type == 'p') {//draw the path cell
+                   mazeCells[r][c].draw(gl, pathColor);
+               } else if(maze[r][c].Type == 's') {//draw the survivor
+                  mazeCells[r][c].draw(gl, survivorColor);
+               }
+           }
+        }
     }
 
     public void onSurfaceChanged(GL10 gl, int width, int height) {
